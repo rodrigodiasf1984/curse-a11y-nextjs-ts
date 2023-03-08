d364902435e18dbce4532e8159cf00bb7f9dfddb
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-03-08 22:50:26 +0100

    chore: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,44 @@
+import Image from 'next/image'
+import LogoImg from '../assets/logo.svg'
+import GithubImg from '../assets/github.svg'
+import styles from '../styles/home.module.css'
+
 export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div>
+      <div className={styles.header}>
+        <Image src={LogoImg} width={143} height={39} />
+
+        <div className={styles.nav}>
+          <a href='https://github.com/rodrigodiasf1984'>
+            <Image src={GithubImg} width={24} height={24} />
+          </a>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <h2>Desenvolendo uma web accessivel</h2>
+        <h4>
+          Protocolos e diretrizes orientam o desenvolvimento de tecnologias
+          acessíveis, mas é preciso olhar para além de tudo isso
+        </h4>
+        <p>
+          Acessibilidade se tornou uma tendência no ecossistema tecnológico
+          mundial, diversas empresas passaram a adotar critérios de
+          desenvolvimento acessível em seus projetos — por uma questão prática
+          ou por exigências legais —, no entanto, ainda faltam recursos e
+          consciência do que de fato é inclusivo na internet para pessoas com
+          deficiências.
+        </p>
+        <p>
+          Para abordar este tema, é preciso ter uma perspectiva cultural sobre o
+          assunto, não encará-lo apenas na ótica técnica ou prática.
+          Acessibilidade passou a ser vista como um desafio (ou tendência) para
+          o ecossistema tech e não como filosofia natural daquilo que condiz com
+          os princípios básicos da web: acessível para humanos. Tal afirmação
+          está na W3C.
+        </p>
+        <h3>O que é acessibilidade, afinal?</h3>
+      </div>
+    </div>
   )
 }
